786cd3ff793a1064902b32595dca5b607db2283a
Ahmed Amin <ahmedamin1700@gmail.com>
2024-02-14 22:04:06 +0200

    Day 2: Cube Conundrum -- first task solved.

---
+++ Cube-Conundrum/cube.py
@@ -0,0 +1,49 @@
+COLORS_MAP = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+
+filename = "input.txt"
+
+file = open(filename, "r")
+lines = file.readlines()
+
+
+def generate_games(data):
+    gs = []
+
+    for line in data:
+        data = line.split(":")[1].strip()
+
+        game = [
+            [
+                [
+                    entry.strip() for entry in trial.strip().split(" ")
+                ] for trial in row.strip().split(",")
+            ] for row in data.split(";")
+        ]
+        gs.append(game)
+    return gs
+
+
+def count_valid_ids(plays):
+    count = 0
+
+    for i, game in enumerate(plays):
+        win = True
+        for trial in game:
+            for entry in trial:
+                if COLORS_MAP[entry[1]] < int(entry[0]):
+                    win = False
+                    break
+
+            if not win:
+                break
+        if win:
+            count += i + 1
+    return count
+
+
+games = generate_games(lines)
+result = count_valid_ids(games)
